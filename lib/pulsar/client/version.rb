dabc4351b35687222600f164d0e5cea64367edcf
tison <wander4096@gmail.com>
2022-08-20 00:35:17 +0800

    initailize
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ lib/pulsar/client/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Pulsar
+  module Client
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/pulsar/client_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Pulsar::Client do
+  it "has a version number" do
+    expect(Pulsar::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
